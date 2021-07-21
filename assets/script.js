73fcbbc235b45cefc6388bf2e868c4b10ecc27a5
Hannah McDonald <hannahnmcdonald@protonmail.com>
2021-07-21 14:37:44 -0400

    changed some fonts/styling again

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,12 @@
-var today = moment();
-$("#currentDay").text(today.format("dddd, MMMM Do YYYY, h:mm a"));
 
+// Below is moment.js function to auto-update time on webpage----------//
+var update = function () {
+    date = moment(new Date())
+    currentDay.html(date.format('dddd, MMMM Do YYYY, h:mm:ss a'));
+};
+
+$(document).ready(function(){
+    currentDay = $('#currentDay')
+    update();
+    setInterval(update, 1000);
+});
