a8fec076a6eaab3c121974d0e9c391beed97811a
Hannah McDonald <hannahnmcdonald@protonmail.com>
2021-07-22 10:01:39 -0400

    changed hour options

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,6 @@
 var saveBtn = document.getElementsByClassName('saveBtn');
 var timeContainer = document.getElementsByClassName('timeContainer');
 // Hour Variables: -------------------------------------------------------------//
-var hour8 = document.getElementById('hour8');
 var hour9 = document.getElementById('hour9');
 var hour10 = document.getElementById('hour10');
 var hour11 = document.getElementById('hour11');
@@ -12,16 +11,12 @@ var hour14 = document.getElementById('hour14');
 var hour15 = document.getElementById('hour15');
 var hour16 = document.getElementById('hour16');
 var hour17 = document.getElementById('hour17');
-var hour18 = document.getElementById('hour18');
-var hour19 = document.getElementById('hour19');
-var hour20 = document.getElementById('hour20');
-var hour21 = document.getElementById('hour21');
-var hour22 = document.getElementById('hour22');
+
 
 // Below is moment.js to auto-update time on webpage----------//
 var update = function () {
     date = moment(new Date())
-    currentDay.html(date.format('dddd, MMMM Do YYYY, h:mm:ss a'));
+    currentDay.html(date.format('dddd, MMMM Do YYYY, HH:mm:ss a'));
 };
 
 $(document).ready(function(){
@@ -32,8 +27,15 @@ $(document).ready(function(){
 
 // Moment.js  to highlight past/present/future timeContainers------------------//
 
-$( "saveBtn" ).click(function() {
-    $( "saveBtn" ).click();
-  });
+// if (moment(data.update_script, 'YYYY-MM-DD h:mm:ss') < moment()) {
+
+
+
+// $( "saveBtn" ).click(function() {
+//     $( "saveBtn" ).click();
+//   });
+
+
+
 
 // Add input to local storage--------------------------------------------------//
