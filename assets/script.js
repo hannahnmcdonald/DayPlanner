a64271a58fe5e31d669cf20fbf5ca01d1433c455
Hannah McDonald <hannahnmcdonald@protonmail.com>
2021-07-22 22:30:05 -0400

    able to get colored elements for past/present/future working, but added more rows because ran out of time and working later at night. Will delete extra rows tomorrow. time for bed

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,7 @@
 // Declare Variables:-----------------------------------------------------------//
 var saveBtn = document.getElementsByClassName('saveBtn');
-var timeContainer = document.getElementsByClassName('timeContainer');
-// Hour Variables: -------------------------------------------------------------//
-var hour9 = document.getElementById('hour9');
-var hour10 = document.getElementById('hour10');
-var hour11 = document.getElementById('hour11');
-var hour12 = document.getElementById('hour12');
-var hour13 = document.getElementById('hour13');
-var hour14 = document.getElementById('hour14');
-var hour15 = document.getElementById('hour15');
-var hour16 = document.getElementById('hour16');
-var hour17 = document.getElementById('hour17');
-
+var btnContainer = document.getElementsByClassName('btnContainer');
+var time = document.getElementsByClassName('time');
 
 // Below is moment.js to auto-update time on webpage----------//
 var update = function () {
@@ -25,17 +15,34 @@ $(document).ready(function(){
     setInterval(update, 1000);
 });
 
+
 // Moment.js  to highlight past/present/future timeContainers------------------//
+const rows = document.getElementsByClassName("row");
+let currentHour = parseInt(moment().format('HH'));
 
-// if (moment(data.update_script, 'YYYY-MM-DD h:mm:ss') < moment()) {
+Array.from(rows).forEach(row => {
+  let
+    rowIdString = row.id,
+    rowHour;
+  if (rowIdString) {
+    rowHour = parseInt(rowIdString);
+  }
+  if (rowHour) {
+    // Compares row id to current hour and sets color accordingly
+    if (currentHour === rowHour) {
+      setColor(row, "rgb(181,88,110, 0.55");
+    } else if (currentHour < rowHour) {
+      setColor(row, "rgb(255,255,255,0.45");
+    } else if (currentHour > rowHour) {
+      setColor(row, "rgb(85,46,109, 0.45)");
+    }
+  }
+});
+
+function setColor(element, color) {
+  element.style.backgroundColor = color;
+};
+
+// // Add input to local storage--------------------------------------------------//
 
 
-
-// $( "saveBtn" ).click(function() {
-//     $( "saveBtn" ).click();
-//   });
-
-
-
-
-// Add input to local storage--------------------------------------------------//
