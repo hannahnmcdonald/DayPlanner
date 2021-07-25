c2083bbdc59beacb795ce33bb341c9bf7245dd40
Hannah McDonald <hannahnmcdonald@protonmail.com>
2021-07-24 22:45:02 -0400

    not fixed yet...still working on some things. Routine git push

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,17 @@
-// Moment.js to auto-update time on webpage----------//
+// Moment.js to auto-update time on webpage------------------------------------------//
 var update = function () {
-    date = moment(new Date())
+    date = moment(new Date());
     currentDay.html(date.format('dddd, MMMM Do YYYY, HH:mm:ss a'));
 };
 
 $(document).ready(function(){
-    currentDay = $('#currentDay')
+    currentDay = $('#currentDay');
     update();
     setInterval(update, 1000);
 });
 
 
-// Moment.js  to highlight past/present/future timeContainers------------------//
+// Moment.js  to highlight past/present/future timeContainers----------------------//
 
 const rows = document.getElementsByClassName("row");
 let currentHour = parseInt(moment().format('HH'));
@@ -24,7 +24,7 @@ Array.from(rows).forEach(row => {
     rowHour = parseInt(rowIdString);
   }
   if (rowHour) {
-    // Compares row id to current hour and sets color accordingly
+    // Compares row id to current hour and sets color with the setColor(); function----//
     if (currentHour === rowHour) {
       setColor(row, "rgb(181,88,110, 0.55");
     } else if (currentHour < rowHour) {
@@ -35,27 +35,38 @@ Array.from(rows).forEach(row => {
   }
 });
 
+// Set color function below----------------------------------------------------------//
 function setColor(element, color) {
   element.style.backgroundColor = color;
 };
 
-// Add input to local storage--------------------------------------------------//
+// Add input to local storage-------------------------------------------------------//
 
 const button = document.querySelector('.saveBtn');
-const input = document.getElementById('h17input', 'h16input','h15input','h14input','h13input','h12input','h11input','h10input','h9input');
+const hours = ['9', '10' , '11', '12' , '13', '14', '15', '16', '17'];
+let input = document.getElementById('input');
 
-button.addEventListener('click', savePlan());
 
 function savePlan() {
-  localStorage.setItem('plan', input.value);
-}
 
+    for (let i = 0; i < hours.length; i++) {
+        console.log(hours[i]);
+        // console.log(input);
+    };
+    localStorage.setItem('plan', JSON.stringify(hours));
+    // console.log(plan);
+    
+};
+
+button.addEventListener('click', function(e) {
+    e.preventDefault();
+    savePlan()
+
+});
+
+// Retrieves plan from local Storage-----------------------------------------------//
 function getPlan() {
-  return localStorage.getItem('plan');
-}
+    return localStorage.getItem(JSON.parse('plans'));
+};
 
 getPlan();
-
-// Test:
-// console.log(h16input.value);
-// console.log(h12input.value);
